2b42ef8c081316003ea4a9fdce66b32e884e7e4f
Enrico Marino <enrico.marino@email.com>
2012-09-19 10:23:25 +0200

    added matrix3.create

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-module.exports = require('./lib/matrix3.js');
+/* !
+ * matrix3
+ * JavaScript math library for 3d matrix
+ * Copyright (c) 2012 Enrico Marino (http://onirame.no.de)
+ * MIT License
+ */
+
+ !(function (exports) {
+
+  /**
+   * Library namespace.
+   */
+
+  var matrix3 = exports.matrix3 = {};
+
+  /**
+   * Library version.
+   */
+
+  matrix3.version = '0.0.0';
+
+  /**
+   * create
+   * Create a 3d matrix.
+   * 
+   * @param {Float32Array} m values
+   * @return {Float32Array} 2d matrix
+   * @api public
+   */
+
+  matrix3.create = function (m) {
+    var values = m || [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0
+      ];
+
+    return new Float32Array(values);
+  };
+
+ }(this));
